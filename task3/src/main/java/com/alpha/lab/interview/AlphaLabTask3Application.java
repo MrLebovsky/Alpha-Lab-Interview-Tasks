e328944177853482289e20e48513c1754d76b78a
RKrasnoslabodtsev <RKrasnoslabodtsev@bellimtegrator.com>
2022-06-27 22:14:47 +0700

    3rd task impl

---
+++ task3/src/main/java/com/alpha/lab/interview/AlphaLabTask3Application.java
@@ -0,0 +1,27 @@
+package com.alpha.lab.interview;
+
+
+import com.alpha.lab.interview.algorithms.PrimeNumberService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaLabTask3Application implements CommandLineRunner {
+
+    private final PrimeNumberService primeNumberService;
+
+    public AlphaLabTask3Application(PrimeNumberService primeNumberService) {
+        this.primeNumberService = primeNumberService;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlphaLabTask3Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        primeNumberService.sequentialAlgorithm();
+        primeNumberService.parallelStreamAlgorithm();
+    }
+}
